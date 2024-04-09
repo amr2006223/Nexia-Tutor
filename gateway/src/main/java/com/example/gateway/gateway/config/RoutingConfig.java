330fdf2594236768b2fb8e39fdeb7228a6738f2a
Ramez-sherif <ramessherif@gmail.com>
2024-04-09 12:21:35 +0200

    gateway updated routs

--- gateway/src/main/java/com/example/gateway/gateway/config/RoutingConfig.java
+++ gateway/src/main/java/com/example/gateway/gateway/config/RoutingConfig.java
@@ -1,6 +1,5 @@
 package com.example.gateway.gateway.config;
 
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +9,20 @@ import org.springframework.context.annotation.Configuration;
 public class RoutingConfig {
     @Bean
     public RouteLocator configureRoute(RouteLocatorBuilder builder) {
-        //example for front end url to nexia-tutor port
+        // example for front end url to nexia-tutor port
         // http://localhost:8080/nexia-tutor/api/auth/gentoken/123321
         return builder.routes()
-                .route("ReportId", r -> r.path("/report/**").uri("http://localhost:9560")) // Route to Report Generation
-                                                                                           // service
-
-                .route("nexia-tutor", r -> r.path("/nexia-tutor/**").uri("lb://nexia-tutor")) // Route to Nexia-Tutor service
-                .route("ScreenId", r -> r.path("/screening/**").uri("http://localhost:8000"))// Route to screening
-                                                                                             // service
-                .route("Scraping", r -> r.path("/scraping/**").uri("http://localhost:8665"))// Route to webscraping
+                .route("ReportId", r -> r.path("/report/**").uri("lb://report-generation")) // Route to Report
+                                                                                            // Generation
                                                                                             // service
+
+                .route("nexia-tutor", r -> r.path("/nexia-tutor/**").uri("lb://nexia-tutor")) // Route to Nexia-Tutor
+                                                                                              // service
+                .route("ScreenId", r -> r.path("/screening/**").uri("lb://Screening_service"))// Route to screening
+                                                                                              // service
+                .route("Scraping", r -> r.path("/scraping/**").uri("lb://Scraping_service"))// Route to webscraping
+                .route("Scraping", r -> r.path("/extracting/**").uri("lb://Extraction_service ")) // service
+
                 .build();
     }
 }
