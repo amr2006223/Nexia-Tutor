efd2b986002d18312a47cff4d3368d3e0ac1ed21
Ramez-sherif <ramessherif@gmail.com>
2024-05-10 21:48:03 +0300

    eureka doc

--- eurekaserver/src/main/java/com/example/eurekaserver/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/example/eurekaserver/eurekaserver/EurekaserverApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+/**
+ * Indicates that this Spring Boot application is serving as a Eureka server.
+ * This annotation enables the Eureka server functionality,
+ * allowing the application to register other microservices and manage service
+ * discovery.
+ */
 @EnableEurekaServer
 public class EurekaserverApplication {
 
