1755356ccca37d06aca93c80540610c952a2309a
Ramez-sherif <ramessherif@gmail.com>
2024-06-24 20:30:10 +0300

    updated split data and train data

--- ScreeningServiceAPI/classification/model_trainer.py
+++ ScreeningServiceAPI/classification/model_trainer.py
@@ -28,14 +28,27 @@ class RandomForestModelTrainer:
     def splitData(self,dataframe):
         y = dataframe['Dyslexia']
         X = dataframe.loc[:, dataframe.columns != 'Dyslexia']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_train = y_train
-        self.y_test = y_test
-        return X_train, X_test, y_train, y_test
+        smote = SMOTE(random_state=42)
+        X_resampled, y_resampled = smote.fit_resample(X, y)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X_resampled, y_resampled, test_size=0.2, random_state=42, stratify=y_resampled
+        )
+        # self.X_train = X_train
+        # self.X_test = X_test
+        # self.y_train = y_train
+        # self.y_test = y_test
+        # return X_train, X_test, y_train, y_test
     
     def trainData(self):
+        # self.model.fit(self.X_train, self.y_train)
+        best_rf_params = {
+            'n_estimators': 200,
+            'max_depth': 40,
+            'min_samples_leaf': 2,
+            'bootstrap': False,
+            'random_state': 42
+        }
+        self.model = RandomForestClassifier(**best_rf_params)
         self.model.fit(self.X_train, self.y_train)
         
     def predictData(self,record):
