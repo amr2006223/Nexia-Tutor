cedb1cc9edf658c3fc447e5c9cabbc7ef155e664
Ramez-sherif <ramessherif@gmail.com>
2024-06-24 21:33:32 +0300

    updated model trainer

--- ScreeningServiceAPI/classification/model_trainer.py
+++ ScreeningServiceAPI/classification/model_trainer.py
@@ -2,17 +2,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from classification.data_preprocessor import DataPreProcessing
 from imblearn.over_sampling import SMOTE
+
 class RandomForestModelTrainer:
-    model = ""
-    X_train = ""
-    X_test= ""
-    y_train= ""
-    y_test = ""
-    
     def __init__(self):
         self.model = RandomForestClassifier()
-     
-    def prepareDataForTraining(self,dataset,dataManipulator):
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.y_pred = None
+
+    def prepareDataForTraining(self, dataset, dataManipulator):
         dataPreProcessing = DataPreProcessing()
         # Preprocess our data
         columns = dataPreProcessing.SeparateColumns(dataset)
@@ -25,7 +25,7 @@ class RandomForestModelTrainer:
         self.splitData(desktopData)
         return cleanedDesktopData
 
-    def splitData(self,dataframe):
+    def splitData(self, dataframe):
         y = dataframe['Dyslexia']
         X = dataframe.loc[:, dataframe.columns != 'Dyslexia']
         smote = SMOTE(random_state=42)
@@ -33,14 +33,8 @@ class RandomForestModelTrainer:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X_resampled, y_resampled, test_size=0.2, random_state=42, stratify=y_resampled
         )
-        # self.X_train = X_train
-        # self.X_test = X_test
-        # self.y_train = y_train
-        # self.y_test = y_test
-        # return X_train, X_test, y_train, y_test
-    
+
     def trainData(self):
-        # self.model.fit(self.X_train, self.y_train)
         best_rf_params = {
             'n_estimators': 200,
             'max_depth': 40,
@@ -50,9 +44,9 @@ class RandomForestModelTrainer:
         }
         self.model = RandomForestClassifier(**best_rf_params)
         self.model.fit(self.X_train, self.y_train)
+        # Predict on the test set
+        self.y_pred = self.model.predict(self.X_test)
         
-    def predictData(self,record):
-        prediction =  self.model.predict(record)
+    def predictData(self, record):
+        prediction = self.model.predict(record)
         return prediction
-    
-        
